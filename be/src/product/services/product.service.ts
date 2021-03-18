ac9202dc26c01506b34438c426465b6f4822ad26
LAPTOP-A492V9GK\ADMIN <cong.nguyenthanhdhbk@gmail.com>
2021-03-18 20:05:35 +0700

    [PP]-Add documents

--- be/src/product/services/product.service.ts
+++ be/src/product/services/product.service.ts
@@ -5,6 +5,7 @@ import {Product} from "../interfaces/product.interface";
 import {RefBookCharacteristics} from "../interfaces/ref-book-characteristics.interface";
 import {ForsageService} from "../../forsage/services/forsage.service";
 import * as moment from "moment";
+import * as momentTz from "moment-timezone";
 import * as _ from "lodash";
 import {Cron} from "@nestjs/schedule";
 
@@ -18,20 +19,31 @@ export class ProductService {
         private readonly forsageService: ForsageService
     ) {}
 
-    @Cron("0 */5 * * * *")
-    async getProducts(): Promise<Product> {
+    @Cron("*/60 * * * * *")
+    async getProducts(): Promise<void> {
         this.logger.debug("*******Start get product*********")
-        const now = moment();
-        const startDate = now
-            .subtract(3, "days")
+        const now = momentTz().tz("Europe/Kiev");
+        const firstDay = momentTz("2020/01/04").tz("Europe/Kiev");
+        const startDate = firstDay
+            .subtract(2, "days")
             .startOf("day")
             .unix();
-        const endDate = now.endOf("day").unix();
+        const endDate = now
+            .endOf("day")
+            .unix();
         const products = await this.forsageService.getProducts(startDate, endDate, 1);
         const { status, data } = products;
         if (_.isEqual(status, 200)) {
-            return await this.productModel.insertMany(data?.products);
+            const pds = await Promise.all(data?.products.map(async (product) => {
+                const productExists = await this.productModel.findOne({ id: product.id});
+                if (productExists) {
+                    return null;
+                }
+                return product;
+            }).filter((filter) => !_.isEqual(filter, null)));
+            // @ts-ignore
+            await this.productModel.insertMany(pds);
+            this.logger.debug(`import successfully ${pds.length} records`);
         }
-        return null;
     }
 }
