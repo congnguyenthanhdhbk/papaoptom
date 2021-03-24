0e2843b2b7c15be0d9c75a3f3f1f4b0788decc72
LAPTOP-A492V9GK\ADMIN <cong.nguyenthanhdhbk@gmail.com>
2021-03-25 00:11:09 +0700

    [PP] - Add port 8888

--- be/src/main.ts
+++ be/src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   const PORT = process.env.PORT || 3000;
-  await app.listen(3000);
+  await app.listen(PORT);
   warn(`APP IS LISTENING TO PORT ${PORT}`);
 }
 bootstrap();
