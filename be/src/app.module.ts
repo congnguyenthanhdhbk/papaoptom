afb081d237ec91f4a7b956c410b50bb113676a4b
Cong Nguyen Thanh <57513462+congnguyenthanhdhbk@users.noreply.github.com>
2021-03-25 16:17:28 +0700

    [NVP] - Add graphl

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -3,23 +3,25 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import {MongooseModule} from "@nestjs/mongoose";
-import {ConfigModule} from "@nestjs/config";
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { ProductModule } from './product/product.module';
 import { ForsageModule } from './forsage/forsage.module';
-import {ScheduleModule} from "@nestjs/schedule";
-import {CsvModule} from "nest-csv-parser";
+import { ScheduleModule } from '@nestjs/schedule';
+import { CsvModule } from 'nest-csv-parser';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
   imports: [
-      ConfigModule.forRoot(),
-      ScheduleModule.forRoot(),
-      MongooseModule.forRoot(process.env.MONGO_URI),
-      AuthModule,
-      UserModule,
-      ProductModule,
-      ForsageModule,
-      CsvModule,
+    ConfigModule.forRoot(),
+    ScheduleModule.forRoot(),
+    GraphQLModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    AuthModule,
+    UserModule,
+    ProductModule,
+    ForsageModule,
+    CsvModule,
   ],
   controllers: [AppController],
   providers: [AppService],
