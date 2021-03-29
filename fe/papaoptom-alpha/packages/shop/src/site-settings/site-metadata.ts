ea15f856d69afc275874a1b4fb62ba86eafc2252
NOVALAND\anh.nguyenquangle <anh.nguyenquangle@novaland.com.vn>
2021-03-29 13:52:13 +0700

    Init FE project - Alpha version

---
+++ fe/papaoptom-alpha/packages/shop/src/site-settings/site-metadata.ts
@@ -0,0 +1,12 @@
+export const siteMetadata = {
+  title: `Papaoptom`,
+  author: {
+    name: `Papaoptom`,
+    summary: ``,
+  },
+  description: ``,
+  siteUrl: ``,
+  social: {
+    twitter: ``,
+  },
+};
---
+++ fe/papaoptom-alpha/packages/shop/src/site-settings/site-translation/messages.ts
@@ -0,0 +1,17 @@
+import localEn from './lang/en.json';
+import localAr from './lang/ar.json';
+import localEs from './lang/es.json';
+import localDe from './lang/de.json';
+import localCn from './lang/zh.json';
+import localIl from './lang/he.json';
+import localRu from './lang/ru.json';
+
+export const messages = {
+  en: localEn,
+  ar: localAr,
+  es: localEs,
+  de: localDe,
+  zh: localCn,
+  he: localIl,
+  ru: localRu,
+};
