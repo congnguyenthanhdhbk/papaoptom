80cd1b867d3d007bd03f068adc1fc31c2a488de9
Cong Nguyen Thanh <57513462+congnguyenthanhdhbk@users.noreply.github.com>
2021-04-01 09:56:14 +0700

    [PP] - refactory code

--- be/src/product/resolvers/product/product.resolver.ts
+++ be/src/product/resolvers/product/product.resolver.ts
@@ -1,18 +1,17 @@
-import { Args, Int, Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { ProductFilterReq } from '../../dto/req/ProductFilterReq';
 import { ProductRes } from '../../dto/res/ProductRes';
 import { HttpStatus } from '@nestjs/common';
 import { ProductService } from '../../services/product.service';
 import * as _ from 'lodash';
-import { CurrencyRate } from '../../../shared/CurrencyRate';
 import { ProductDetailRes } from '../../dto/res/ProductDetailRes';
-import { Product } from '../../interfaces/product.interface';
+import { ProductHelper } from '../../helpers/ProductHelper';
 
 @Resolver()
 export class ProductResolver {
   constructor(
     private readonly productService: ProductService,
-    private readonly currency: CurrencyRate,
+    private readonly productHelper: ProductHelper,
   ) {}
 
   @Query(() => ProductDetailRes)
@@ -32,7 +31,7 @@ export class ProductResolver {
       };
     }
 
-    const characteristics = this.convertPrice(product);
+    const characteristics = this.productHelper.convertPrice(product);
 
     return {
       code: HttpStatus.OK,
@@ -62,22 +61,9 @@ export class ProductResolver {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const product = await this.productService.filterProduct(filter);
-    const data = product?.docs.map((product) => {
-      const characteristics = this.convertPrice(product);
-      return {
-        id: product?.id ?? 0,
-        vcode: product?.vcode ?? null,
-        name: product?.name ?? null,
-        quantity: product?.quantity ?? 0,
-        category: product?.category ?? {},
-        supplier: product?.supplier ?? {},
-        brand: product?.brand ?? {},
-        createdAt: product?.createdAt ?? new Date(),
-        updatedAt: product?.updatedAt ?? new Date(),
-        slug: product?.sku ?? null,
-        characteristics,
-      };
-    });
+    const data = product?.docs.map((product) =>
+      this.productHelper.presentProduct(product),
+    );
     if (product !== null) {
       return {
         code: HttpStatus.OK,
@@ -96,64 +82,6 @@ export class ProductResolver {
     return {
       code: HttpStatus.NOT_FOUND,
       message: 'результатов не найдено',
-    };
-  }
-
-  convertPrice(product: any): any {
-    const characteristic = product?.characteristics ?? {};
-    const type =
-      product?.characteristics?.purchaseCurrency ?? this.currency.CURRENCY_RHA;
-
-    const purchasePrice = this.currency.convertCurrency({
-      type,
-      value: product?.characteristics?.purchasePrice ?? 0,
-    });
-    const sellingPrice = this.currency.convertCurrency({
-      type,
-      value: product?.characteristics?.sellingPrice ?? 0,
-    });
-    const oldPurchasePrice = this.currency.convertCurrency({
-      type,
-      value: product?.characteristics?.oldPurchasePrice ?? 0,
-    });
-    const oldSellingPrice = this.currency.convertCurrency({
-      type,
-      value: product?.characteristics?.oldSellingPrice ?? 0,
-    });
-    const steamInBox = product?.characteristics?.steamInBox ?? 1;
-
-    const totalPurchasePrice = Number(purchasePrice) * Number(steamInBox);
-    const totalSellingPrice = Number(sellingPrice) * Number(steamInBox);
-    const totalOldPurchasePrice = Number(oldPurchasePrice) * Number(steamInBox);
-    const totalOldSellingPrice = Number(oldSellingPrice) * Number(steamInBox);
-    // TODO: Add percent
-    const subtractSellingPrice = _.subtract(
-      totalSellingPrice,
-      totalOldSellingPrice,
-    );
-    const discountInPercent = _.round(
-      _.divide(subtractSellingPrice, totalSellingPrice) * 100,
-      0,
-    ).toFixed(0);
-    const gallery = [];
-    const url = { url: product?.characteristics?.photo1 ?? null };
-    gallery.push(url);
-    gallery.push(url);
-    gallery.push(url);
-    gallery.push(url);
-
-    return {
-      ...characteristic,
-      purchasePrice,
-      sellingPrice,
-      oldPurchasePrice,
-      oldSellingPrice,
-      totalOldPurchasePrice,
-      totalOldSellingPrice,
-      totalPurchasePrice,
-      totalSellingPrice,
-      discountInPercent,
-      gallery,
     };
   }
 }
